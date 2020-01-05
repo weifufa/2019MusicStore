48e5ff34b41dd6acf483e9c72aefecef4de71910
Administrator <Administrator@jz701301>
2020-01-05 18:11:57 +0800

    WxPayAPI

---
+++ cs_sdk_v3.0.9/WxPayAPI/lib/WxPayNativeModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WxPayAPI.lib
+{
+    public class WxPayNativeModel
+    {
+        public string body;
+        public string attach;
+        public string pout_trade_no;
+        public string total_fee;
+        public string time_start;
+        public string time_expire;
+        public string goods_tag;
+        public string trade_type;
+        public string product_id;
+
+        //public WxPayNativeModel MapfJson(string stringJson)
+        //{
+        //    return stringJson;
+          
+        //}
+    }
+}
